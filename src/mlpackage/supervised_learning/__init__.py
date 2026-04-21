c62e8ba08ed2627af24a9b82319ac81d6303a5fd
Mia Xie <myx1@rice.edu>
2026-04-21 16:09:13 -0500

    forgot to commit something

--- src/mlpackage/supervised_learning/__init__.py
+++ src/mlpackage/supervised_learning/__init__.py
@@ -8,10 +8,14 @@ from mlpackage.supervised_learning.decision_tree_regressor import (
     DecisionTreeRegressor,
     RandomForestRegressor,
 )
+from mlpackage.supervised_learning.knn import KNeighborsClassifier
+from mlpackage.supervised_learning.linear_regression import LinearRegression
 
 __all__ = [
     "DecisionTreeClassifier",
     "RandomForestClassifier",
     "DecisionTreeRegressor",
     "RandomForestRegressor",
+    "KNeighborsClassifier",
+    "LinearRegression",
 ]
